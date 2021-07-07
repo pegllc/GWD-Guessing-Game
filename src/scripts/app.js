ede8abbd32da37559232d68f646a9471f6108a7b
GabrielGerhauser <84334180+GabrielGerhauser@users.noreply.github.com>
2021-07-07 14:53:55 -0400

    Finishing touches

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,34 +2,51 @@ const userInput = document.getElementById("userInput");
 const submitButton = document.getElementById("btnSubmit");
 const answerParagraph = document.getElementById("answer");
 let attemptsleft = 2;
-let stillrunning = true; 
+let stillrunning = true;
+let magicnumber=7;
 submitButton.addEventListener("click", function () {
     if(!stillrunning){return;}
     let userResponse = userInput.value;
     
-    if (attemptsleft != 0) {
-        if (userResponse >= 1 && 10 >= userResponse) {
-            if (userResponse == 7) {
+    if (attemptsleft != 0)
+    {
+        if (userResponse >= 1 && 10 >= userResponse)
+        {
+            if (userResponse == magicnumber)
+            {
                 answerParagraph.innerText = "You Won Sun";
             }
-            
-            else {
-                answerParagraph.innerText = "Try Again Please" +attemptsleft;
+            else
+            {
                 attemptsleft--;
+                let HigherOrLower="";
+                if(userResponse<magicnumber)
+                {
+                    HigherOrLower="Higher";
+                }
+                else if(userResponse>magicnumber)
+                {
+                    HigherOrLower="Lower";
+                }
+                answerParagraph.innerText = "Try Again Please. "+attemptsleft+" remaining. Secret number is "+HigherOrLower;
             }
 
         }
-        else if (userResponse == 0) {
+        else if (userResponse == 0)
+        {
             answerParagraph.innerText = "Enter a number between 1 and 10";
-        } else if (userResponse == -1) {
-            answerParagraph.innerText = "Terminated";
-            stillrunning = false;
         }
-
     }
-    else {
+    else
+    {
         answerParagraph.innerText = "You outta attempts dawg";
-     }
+    }
+    if (userResponse == -1)
+    {
+        answerParagraph.innerText = "Terminated";
+        stillrunning = false;
+    }
+    
   
     
     
